90a341cf51224b2477dd3d6e874be9f5d9aaf095
Loris Nanchen <loris.nanchen@edu.vs.ch>
2023-12-13 07:51:07 +0100

    amplify backend addded

---
+++ src/models/schema.js
@@ -0,0 +1,74 @@
+export const schema = {
+    "models": {
+        "Post": {
+            "name": "Post",
+            "fields": {
+                "id": {
+                    "name": "id",
+                    "isArray": false,
+                    "type": "ID",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "titel": {
+                    "name": "titel",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": false,
+                    "attributes": []
+                },
+                "content": {
+                    "name": "content",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": false,
+                    "attributes": []
+                },
+                "createdAt": {
+                    "name": "createdAt",
+                    "isArray": false,
+                    "type": "AWSDateTime",
+                    "isRequired": false,
+                    "attributes": [],
+                    "isReadOnly": true
+                },
+                "updatedAt": {
+                    "name": "updatedAt",
+                    "isArray": false,
+                    "type": "AWSDateTime",
+                    "isRequired": false,
+                    "attributes": [],
+                    "isReadOnly": true
+                }
+            },
+            "syncable": true,
+            "pluralName": "Posts",
+            "attributes": [
+                {
+                    "type": "model",
+                    "properties": {}
+                },
+                {
+                    "type": "auth",
+                    "properties": {
+                        "rules": [
+                            {
+                                "allow": "public",
+                                "operations": [
+                                    "create",
+                                    "update",
+                                    "delete",
+                                    "read"
+                                ]
+                            }
+                        ]
+                    }
+                }
+            ]
+        }
+    },
+    "enums": {},
+    "nonModels": {},
+    "codegenVersion": "3.4.4",
+    "version": "4078753e9d5e0885e817a3d0257cfa0c"
+};
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,10 @@
 import '@/styles/globals.css'
+import { Amplify } from "aws-amplify";
+import config from '../aws-exports';
+
+Amplify.configure({
+  ...config, ssr: true
+});
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
